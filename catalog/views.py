7e6e232a7e571a65b6e838ba84eaaefdaf6c7ca5
Nitesh-Mint <nitesh.raya058@gmail.com>
2024-01-11 19:56:54 +0545

    added some view and urls to show data to the home page of the app

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Book, BookInstance, Author, Genre
+
+def index(request):
+    """Home page view"""
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books 
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The all() is applied default
+    num_authors = Author.objects.count()
+
+    # Books that contain particular word
+    particular_word_book = Book.objects.filter(title__icontains="Atomic").count()
+
+    context = {
+        'num_books':num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors':num_authors,
+        'particular_word_book': particular_word_book,
+    }
+    
+    return render(request, 'index.html', context=context)
