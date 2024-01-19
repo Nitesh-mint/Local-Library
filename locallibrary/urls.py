1f3119a3736bcb120bc41ea6cd7e2ff6d1aae171
Nitesh-Mint <nitesh.raya058@gmail.com>
2024-01-19 23:04:36 +0545

    updated

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -9,3 +9,9 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# For authentication (login, logout, signup )
+
+urlpatterns += [
+    path('accounts  /', include('django.contrib.auth.urls')),
+]
